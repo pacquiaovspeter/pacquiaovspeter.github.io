ca9cf43925763e2f1db3febaa2dd82db59a650de
pacquiaovspeter <100389783+pacquiaovspeter@users.noreply.github.com>
2022-02-25 01:01:07 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const body = document.querySelector('.maincontent')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
 
-const audio = new Audio("images/clock3.mp3");
-audio.loop = true;
-audio.play();
+body.addEventListener('click', function() {
+    const audio = new Audio("images/clock3.mp3");
+    audio.loop = true;
+    audio.play();
+})
